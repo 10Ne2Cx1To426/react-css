a4457372a7a886b1d40cef750c848341b4c5aaa2
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-17 15:41:57 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { CssModules } from "./Components/CssModeles";
+import { Emotion } from "./Components/Emotion";
+import { InlineStyle } from "./Components/InlineStyle";
+import { StyledComponents } from "./Components/StyledComponents";
+import { StyledJsx } from "./Components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
